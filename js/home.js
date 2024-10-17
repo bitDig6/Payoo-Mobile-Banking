e002e4c5e4a9341bb1f7fbc7647fbdf09515bb82
bitDig6 <ummenabilajalal@gmail.com>
2024-10-17 10:47:10 +0600

    Added the event handler for add money button partially

--- js/home.js
+++ js/home.js
@@ -1,1 +1,11 @@
-console.log('home added');
+//console.log('home added');
+
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+   event.preventDefault();
+
+   const addMoneyInput = document.getElementById('input-add-money').value;
+   console.log(addMoneyInput);
+
+   const pinNumberInput = document.getElementById('input-pin-number').value;
+   console.log(pinNumberInput);
+});
