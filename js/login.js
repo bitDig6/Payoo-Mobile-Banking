c296acb44194140294b35744af518e5c73a73c7c
bitDig6 <ummenabilajalal@gmail.com>
2024-10-17 10:22:35 +0600

    added home page, not completed

--- js/login.js
+++ js/login.js
@@ -2,10 +2,36 @@
 
 // structure document.getElementById('btn-login').addEventListener('click', function(){});
 
+// document.getElementById('btn-login').addEventListener('click', function(event){
+//     event.preventDefault();
+//     // console.log('login button clicked');
+
+//     const phoneNumber = document.getElementById('phone-number').value;
+//     const pinNumber = document.getElementById('pin-number').value;
+
+//     console.log(phoneNumber, pinNumber);
+
+//     //this is temporary, you shouldn't do like this
+//     if(phoneNumber === '5' && pinNumber === '1234'){
+//         console.log('You are logged in');
+//     }else{
+//         alert('Wrong Phone Number or Pin');
+//     }
+// });
+
 document.getElementById('btn-login').addEventListener('click', function(event){
     event.preventDefault();
-    console.log('login button clicked');
 
-    const phoneNumber = document.getElementById('phone-number');
-    console.log(phoneNumber.value);
+    //get phone number and pin
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    // console.log(phoneNumber, pinNumber);
+
+    //bad way to validate or verify
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        console.log('You are logged in');
+        window.location.href  = '/home.html'
+    }else{
+        alert('Wrong phone number or pin');
+    }
 });
