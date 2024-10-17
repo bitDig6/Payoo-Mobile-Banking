04cbb55fde0d5059e42ebdced8e077a174468223
bitDig6 <ummenabilajalal@gmail.com>
2024-10-17 11:11:21 +0600

    event handler to add money to the current balance and display update balane

--- js/home.js
+++ js/home.js
@@ -8,4 +8,21 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
 
    const pinNumberInput = document.getElementById('input-pin-number').value;
    console.log(pinNumberInput);
+
+   // wrong way to verify
+   if(pinNumberInput === '1234'){
+    console.log('Adding money to your account');
+
+    const balance = document.getElementById('account-balance').innerText;
+    console.log(balance);
+
+    const addMoneyNumber = parseFloat(addMoneyInput);
+    const balanceNumber = parseFloat(balance);
+    const newBalance = addMoneyNumber + balanceNumber;
+    console.log(newBalance);
+
+    document.getElementById('account-balance').innerText = newBalance;
+   }else{
+    alert('Failed to add money. Please try again');
+   }
 });
